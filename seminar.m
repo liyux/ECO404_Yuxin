e1162493c4a7e7ce701db7e80e3ee47841fcf638
susansayre <ssayre@smith.edu>
2014-10-08 15:54:02 -0400

    simplifying loops
    Moved most of the details in the loops into a separate program and
    restructured some of the code to make this possible.

--- seminar.m
+++ seminar.m
@@ -32,6 +32,7 @@ correlationmatrix=[1 0.6 0.7 0.7 ; 0.6 1 0.5 0.5 ; 0.7 0.5 1 0.9 ; 0.7 0.5 0.9 1
 %to keep values inside a range, generate more than needed, remove those
 %outside, and keep top 1000 numbers. csubset=condd1*find(condd1>x11) is subset,
 %keep csubset(1:1000,1)
+rng(1)
 
 income=normrnd(1.156,0.485,[1.5*numhh,1]); %$1000
 isubset=income(find((income>=0.137)&(income<=2.967)));
@@ -45,6 +46,7 @@ lawnsize = lsubset(1:numhh);
 bath=normrnd(1.635,0.52,[1.5*numhh,1]);
 bsubset=bath(find((bath>=1)&(bath<=3)));
 bath = bsubset(1:numhh);
+
 
 correlatedhh=[income lawnsize bath hsize]*chol(correlationmatrix);
 correlatedhh=sortrows(correlatedhh,1);
@@ -67,70 +69,39 @@ condd = [condd1 ones(numhh,1)*x11 condd2];
 % changing p1
 p11exp = 1.1:0.01:1.3;
 numIterp1 = numel(p11exp);
-qtydIBPp1 = zeros(numhh,numIterp1);
-expIBPp1= zeros(numhh,numIterp1);
+qtydIBPi = zeros(numhh,numIterp1);
+expIBPi= zeros(numhh,numIterp1);
+
 
 for i = 1:numIterp1;
     
-    p11i = p11exp(i);
-    %redefining all variables containing p11
-    d2 = 0.001*(p12-p11exp(i))*x11;
-    pricematrix = [p11exp(i)*ones(numhh,1) p11exp(i)*ones(numhh,1) p12*ones(numhh,1)];
-    d.p11 = p11exp(i).^coef.p1;
-    d.y2 = (income+d2).^coef.yd;
-    condd1 = d.hh.*d.p11.*d.y1;
-    condd2 = d.hh.*d.p12.*d.y2;
-    condd = [condd1 ones(numhh,1)*x11 condd2];
+    p12i = p12;
+    p11i = p11exp(i); 
+    x11i = x11;
     
-    % creating a matrix of binary variables that has the value 1 if the
-    %household belongs in that block, and 0 otherwise. 
-    
-    checkCondd1 = condd1<x11;
-    checkCondd2 = condd2>x11;
-    checkAtKink = ones(numhh,1) - checkCondd1 - checkCondd2;
-    hhBlock = [checkCondd1 checkAtKink checkCondd2];
-    qtydIBPp1(:,i) = sum(hhBlock.*condd,2);
-    
-    % finding total expenditure per household by multiplying prices with
-    % quantity demanded in each block. Total revenue is obtained by summing
-    % the column of household expenditures.
-    
-    expIBPp1(:,i) = sum(hhBlock.*condd.*pricematrix,2);
-    
+    computeQandExp
 end
+
+qtydIBPp1 = qtydIBPi;
+expIBPp1 = expIBPi;
 
 %changing x11
 x11exp = 10:1:20;
 numIterx1 = numel(x11exp);
-qtydIBPx1 = zeros(numhh,numIterx1);
-expIBPx1= zeros(numhh,numIterx1);
+qtydIBPi = zeros(numhh,numIterx1);
+expIBPi= zeros(numhh,numIterx1);
 
 %for p11 = 1.1:0.01:1.3
-for j = 1:numIterx1;
+for i = 1:numIterx1;
     
-    x11i = x11exp(j);
-    %redefining all variables containing x11
-    d2 = 0.001*(p12-p11)*x11exp(j);
-    d.y2=(income+d2).^coef.yd;
-    condd2=d.hh.*d.p12.*d.y2;
-    condd = [condd1 ones(numhh,1)*x11exp(j) condd2];
+    p12i = p12;
+    p11i = p11; 
+    x11i = x11exp(i);
     
-    % creating a matrix of binary variables that has the value 1 if the
-    %household belongs in that block, and 0 otherwise. 
-    
-    checkCondd1 = condd1<x11exp(j);
-    checkCondd2 = condd2>x11exp(j);
-    checkAtKink = ones(numhh,1) - checkCondd1 - checkCondd2;
-    hhBlock = [checkCondd1 checkAtKink checkCondd2];
-    qtydIBPx1(:,i) = sum(hhBlock.*condd,2);
-    
-    % finding total expenditure per household by multiplying prices with
-    % quantity demanded in each block. Total revenue is obtained by summing
-    % the column of household expenditures.
-    
-    expIBPx1(:,i) = sum(hhBlock.*condd.*pricematrix,2);
-    
+    computeQandExp
 end
+qtydIBPx1 = qtydIBPi;
+expIBPx1 = expIBPi;
 
 %move 2 things: to make same revenue at this price, where does the break
 %point have to be?
