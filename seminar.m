ddecff74b48daab95cbf4b133649f944088476a0
liyux <Yuxin@Yuxins-MacBook-Pro.local>
2014-10-07 23:34:11 -0400

    1st experiment completed
    changing p1 works, changing x1 gives a zero matrix

--- seminar.m
+++ seminar.m
@@ -46,11 +46,9 @@ bath=normrnd(1.635,0.52,[1.5*numhh,1]);
 bsubset=bath(find((bath>=1)&(bath<=3)));
 bath = bsubset(1:numhh);
 
-% income=normrnd(1.156,0.485,[numhh,1]); %$1000
-% hsize=normrnd(18.337,5.288,[numhh,1]); %100sqft
-% lawnsize=normrnd(9.877,3.383,[numhh,1]); %1000sqft
-% bath=normrnd(1.635,0.52,[numhh,1]);
 correlatedhh=[income lawnsize bath hsize]*chol(correlationmatrix);
+correlatedhh=sortrows(correlatedhh,1);
+income=correlatedhh(1:numhh,1);
 hhmatrix=[constant*ones(numhh,1) correlatedhh(1:numhh,2) weather*ones(numhh,1) correlatedhh(1:numhh,3) correlatedhh(1:numhh,4) billing*ones(numhh,1)];
 %hhmatrix is hhsizex6
 
@@ -66,21 +64,21 @@ condd1=d.hh.*d.p11.*d.y1; %size numhhx1
 condd2=d.hh.*d.p12.*d.y2; %size numhhx1
 condd = [condd1 ones(numhh,1)*x11 condd2];
 
-p11 = 1.1:0.01:1.3;
-numIter = numel(p11);
-qtydIBP = zeros(numhh,numIter);
-expIBP = zeros(numhh,numIter);
+% changing p1
+p11exp = 1.1:0.01:1.3;
+numIterp1 = numel(p11exp);
+qtydIBPp1 = zeros(numhh,numIterp1);
+expIBPp1= zeros(numhh,numIterp1);
 
-%for p11 = 1.1:0.01:1.3
-for i = 1:numIter;
+for i = 1:numIterp1;
     
-    p11i = p11(i);
+    p11i = p11exp(i);
     %redefining all variables containing p11
-    d2 = 0.001*(p12-p11(i))*x11;
-    pricematrix = [p11(i)*ones(numhh,1) p11(i)*ones(numhh,1) p12*ones(numhh,1)];
-    d.p11 = p11(i).^coef.p1;
+    d2 = 0.001*(p12-p11exp(i))*x11;
+    pricematrix = [p11exp(i)*ones(numhh,1) p11exp(i)*ones(numhh,1) p12*ones(numhh,1)];
+    d.p11 = p11exp(i).^coef.p1;
     d.y2 = (income+d2).^coef.yd;
-    condd1 = d.hh.*d.p11(i).*d.y1;
+    condd1 = d.hh.*d.p11.*d.y1;
     condd2 = d.hh.*d.p12.*d.y2;
     condd = [condd1 ones(numhh,1)*x11 condd2];
     
@@ -91,17 +89,48 @@ for i = 1:numIter;
     checkCondd2 = condd2>x11;
     checkAtKink = ones(numhh,1) - checkCondd1 - checkCondd2;
     hhBlock = [checkCondd1 checkAtKink checkCondd2];
-    qtydIBP(:,i) = sum(hhBlock.*condd,2);
+    qtydIBPp1(:,i) = sum(hhBlock.*condd,2);
     
     % finding total expenditure per household by multiplying prices with
     % quantity demanded in each block. Total revenue is obtained by summing
     % the column of household expenditures.
     
-    expIBP(:,i) = sum(hhBlock.*condd.*pricematrix,2);
+    expIBPp1(:,i) = sum(hhBlock.*condd.*pricematrix,2);
     
 end
 
-%make rows the different simulations and columns income quintiles
+%changing x11
+x11exp = 10:1:20;
+numIterx1 = numel(x11exp);
+qtydIBPx1 = zeros(numhh,numIterx1);
+expIBPx1= zeros(numhh,numIterx1);
+
+%for p11 = 1.1:0.01:1.3
+for j = 1:numIterx1;
+    
+    x11i = x11exp(j);
+    %redefining all variables containing x11
+    d2 = 0.001*(p12-p11)*x11exp(j);
+    d.y2=(income+d2).^coef.yd;
+    condd2=d.hh.*d.p12.*d.y2;
+    condd = [condd1 ones(numhh,1)*x11exp(j) condd2];
+    
+    % creating a matrix of binary variables that has the value 1 if the
+    %household belongs in that block, and 0 otherwise. 
+    
+    checkCondd1 = condd1<x11exp(j);
+    checkCondd2 = condd2>x11exp(j);
+    checkAtKink = ones(numhh,1) - checkCondd1 - checkCondd2;
+    hhBlock = [checkCondd1 checkAtKink checkCondd2];
+    qtydIBPx1(:,i) = sum(hhBlock.*condd,2);
+    
+    % finding total expenditure per household by multiplying prices with
+    % quantity demanded in each block. Total revenue is obtained by summing
+    % the column of household expenditures.
+    
+    expIBPx1(:,i) = sum(hhBlock.*condd.*pricematrix,2);
+    
+end
 
 %move 2 things: to make same revenue at this price, where does the break
 %point have to be?
